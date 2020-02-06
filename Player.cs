9d397fb206b85e5faf789fb6d1adc4c1c34231e3
AndrewWaltman <andrew.w.waltman@gmail.com>
2020-02-05 22:01:43 -0500

    Updates with comments.

--- Player.cs
+++ Player.cs
@@ -4,15 +4,22 @@ using System.Text;
 
 namespace Deliverable_13
 {
+    //I would say the most important Class of the application.
+    //Player is made as an abstract because the Lab asked us to.
     abstract class Player
     {
+        //Stores a Name Value (And required for every other Child Class.)
         public string Name { get; set; }
 
+        //This creates a Roshambo value, makes it virtual so it can be overridden later. The current return doesn't matter basically.
         public virtual Roshambo GetRoshambo()
         {
             return Roshambo.Rock;
         }
     }
+    //Here is the first things asked of us, the enum Roshambo with the values Rock, Paper, and Scissors.
+    //Creating an enum is fairly similar to creating other variables, however, it is an enum and not inheritely a string or int.
+    //When calling an enum, we may have to change the typing for later.
     public enum Roshambo
     {
         Rock,
